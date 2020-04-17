7fe0bf57c16e24453b6f15f28856a92be2e015e0
Julia Fong <Juliamzfong@gmail.com>
2020-04-16 21:49:32 -0500

    Add error messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupForm: NgForm;
+  @ViewChild('f')  signupForm: NgForm;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
